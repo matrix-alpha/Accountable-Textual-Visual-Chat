a623bb1f6efbd716192f8aba2fea87a8093d9116
matrix-alpha <107047197+matrix-alpha@users.noreply.github.com>
2023-06-17 16:56:48 +0800

    Create dist_train_vae.sh

---
+++ dist_train_vae.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -x
+
+PYTHON=${PYTHON:-"python"}
+
+DATA_NAME=$1
+NODES=$2
+GPUS=$3
+
+while true # find unused tcp port
+do
+    PORT=$(( ((RANDOM<<15)|RANDOM) % 49152 + 10000 ))
+    status="$(nc -z 127.0.0.1 $PORT < /dev/null &>/dev/null; echo $?)"
+    if [ "${status}" != "0" ]; then
+        break;
+    fi
+done
+    
+$PYTHON -m torch.distributed.launch --nnodes=${NODES} --nproc_per_node=${GPUS} --node_rank=0 --master_addr="xx.xx.xx.xx" --master_port=$PORT \
+	 train_vae.py --image_folder /data/${DATA_NAME}/
