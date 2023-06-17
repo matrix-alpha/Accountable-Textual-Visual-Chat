aa5e09bab9a6c81fe5f8421584f8f173ece3d58f
matrix-alpha <107047197+matrix-alpha@users.noreply.github.com>
2023-06-17 17:22:23 +0800

    Update gen_vae.sh

--- gen_vae.sh
+++ gen_vae.sh
@@ -6,7 +6,7 @@ PYTHON=${PYTHON:-"python"}
 
 GPU=$1
 VAE_PATH=$2
-IMAGE_PATH=$2
+IMAGE_PATH=$3
 
 CUDA_VISIBLE_DEVICES=${GPU} PYTHON -m torch.distributed.launch --nproc_per_node=1 \
                        /atvc/gen_vae.py --vae_path ${VAE_PATH} --source_image ${IMAGE_PATH}
